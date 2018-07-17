e34159178150d565f3d7bd6ca27931c71e2d6226
Thomas Johnson <tjohnson1106@gmail.com>
2018-07-17 15:05:27 -0400

    created poll controller test

--- lib/vocal_web/controllers/poll_controller.ex
+++ lib/vocal_web/controllers/poll_controller.ex
@@ -2,6 +2,18 @@ defmodule VocalWeb.PollController do
   use VocalWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+
+    poll = %{
+      title: "My First Poll",
+      options: [
+        {"Choice 1", 0},
+        {"Choice 2", 5},
+        {"Choice 3", 2}
+      ]
+    }
+
+   conn
+   |> put_layout(:special)
+   |> render("index.html", poll: poll)
   end
 end
